ce25a717bcfda1716f3249e3cfd495faf920581d
= <=>
2025-09-02 18:23:35 +0300

    implement

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,7 +1,14 @@
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("api/user/", include(("user.urls", "user"), namespace="user")),
-    path("api/cinema/", include(("cinema.urls", "cinema"),
-                                namespace="cinema")),
+    path("admin/", admin.site.urls),
+    path(
+        "api/user/",
+        include(("user.urls", "user"), namespace="user"),
+    ),
+    path(
+        "api/cinema/",
+        include(("cinema.urls", "cinema"), namespace="cinema"),
+    ),
 ]
--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,15 @@ from .views import RegisterView, LoginView, MeView
 app_name = "user"
 
 urlpatterns = [
+    # реєстрація
     path("register/", RegisterView.as_view(), name="register"),
+    path("create/", RegisterView.as_view(), name="create"),
+
+    # отримання токена
     path("login/", LoginView.as_view(), name="login"),
+    path("token/", LoginView.as_view(), name="token"),
+
+    # профіль користувача
     path("me/", MeView.as_view(), name="me"),
+    path("manage/", MeView.as_view(), name="manage"),
 ]
